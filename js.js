642f0188ca9728fb5372d23e915ae03667dee5c8
Katiasfihi <Katiasfihi@users.noreply.github.com>
2021-02-28 20:04:04 +0000

    2nd commit

---
+++ js.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(e.keyCode, audio);
+
+    if(!audio) return;
+
+    audio.play();
+ });
